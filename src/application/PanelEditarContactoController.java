2c3a99d6e64cd064547cebc6d458ed0ba995331d
unknown <yic00732@educastur.es>
2024-11-12 14:59:05 +0100

    cambio de contacto

--- src/application/PanelEditarContactoController.java
+++ src/application/PanelEditarContactoController.java
@@ -2,6 +2,8 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.ResourceBundle;
@@ -46,11 +48,11 @@ public class PanelEditarContactoController extends AnchorPane implements Initial
 	@FXML
 	private RadioButton radio_apache;
 	@FXML
-	private ChoiceBox<String> grupo_;
+	private ChoiceBox<String> grupocombo;
 	@FXML
 	private TextField notas_ad;
 	@FXML
-	private DatePicker fecha_nac;
+	private DatePicker fechanac;
 	//meterunimageview
 	@FXML
 	private Button modificar;
@@ -60,8 +62,7 @@ public class PanelEditarContactoController extends AnchorPane implements Initial
 	private Button atras;
 	
 	private ToggleGroup t =new ToggleGroup();
-	
-	
+		
 	public PanelEditarContactoController(){
 		 System.out.println("estamos en el constructor del panel");
 	        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("paneleditarcontacto.fxml"));
@@ -76,15 +77,12 @@ public class PanelEditarContactoController extends AnchorPane implements Initial
 	        }
 	    }
 
-		
-		
-	
-
 	public ObservableList<Contacto> CargarDatos() {
 		ObservableList<Contacto> con = FXCollections.observableArrayList();
 		con=ControladorContactos.LeerContactos();
 		return con;
 	}
+	
 	@FXML
 	public void ListaDeContactos() {
 		System.out.println("pasa por aqui tiene los datos");
@@ -115,43 +113,123 @@ public class PanelEditarContactoController extends AnchorPane implements Initial
 				 campo_email.setText(con.getEmail());
 				 if(con.getGenero().equalsIgnoreCase(radio_masculino.getText())) {
 					 radio_masculino.setSelected(true);
+					 radio_femenino.setSelected(false);
+					 radio_apache.setSelected(false);
 				 }
 				 if(con.getGenero().equalsIgnoreCase(radio_femenino.getText())) {
 					 radio_femenino.setSelected(true);
+					 radio_masculino.setSelected(false);
+					 radio_apache.setSelected(false);
 				 }
 				 if(con.getGenero().equalsIgnoreCase(radio_apache.getText())){
 					 radio_apache.setSelected(true);
+					 radio_masculino.setSelected(false);
+					 radio_femenino.setSelected(false);
 				 }
-				 
+				 notas_ad.setText(con.getNotas());
+				 DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+				 String fec =con.getFechanac().toString();
+				 fechanac.setValue(LocalDate.parse(fec, formatter));
+				 boolean fav=con.isFavorito();
+				 if(fav) {
+					 esfavorito.setSelected(fav);
+				 }
+				 else {
+					 esfavorito.setSelected(fav);
+				 }
 			 }
 			 System.out.println("llega al final");
 		}
 		else {
 			System.out.println("no se han cargado los datos del contacto");
-		}
-		
+		}		
 	}
+	
 	@FXML
-	public void GrupoGenero() {
+	public String GrupoGenero() {
 		radio_masculino.setText("Masculino");
 		radio_femenino.setText("Femenino");
 		radio_apache.setText("Apache_de_combate");
 		radio_masculino.setToggleGroup(t);
 		 radio_femenino.setToggleGroup(t);
 		 radio_apache.setToggleGroup(t);
+		 RadioButton selec = (RadioButton) t.getSelectedToggle();
+		 String valor =selec.getText();
+		 return valor;
 	}
+	
 	@FXML
 	 public void grupo_combobox() {
-		 grupo_.setItems(FXCollections.observableArrayList("Amigos", "Familia", "trabajo"));
-		 grupo_.setValue("Amigos");
+		 grupocombo.setItems(FXCollections.observableArrayList("Amigos", "Familia", "trabajo"));
+		 grupocombo.setValue("Amigos");
 		 handleListView();
 	 }
+	
 	 @FXML
 	 private void handleListView() {
-		 grupo_.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+		 grupocombo.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
 				System.out.println("Elemento seleccionado: " + newValue);
 			});
 		}
 
-	
+	 @FXML
+	 public void GuardarModificacion() {
+		 ObservableList<Contacto> listacontactos = FXCollections.observableArrayList();
+		 listacontactos=CargarDatos();
+		 Contacto con;
+		 if(lista.getSelectionModel().getSelectedItem()!=null) {
+			 con =lista.getSelectionModel().getSelectedItem();
+			Alert datos = new Alert(AlertType.CONFIRMATION);
+			 datos.setContentText("Quiere Modificar los datos del contacto: "+con.getNombre());
+			 datos.setTitle("Guardar Contacto Modificado");
+			 Optional<ButtonType> result = datos.showAndWait();
+			 if(result.get()==ButtonType.OK) {
+				 Contacto modificado=new Contacto();
+				 String nombre=campo_nombre.getText();
+				 String telefono =campo_telefono.getText();
+				 String email=campo_email.getText();
+				 boolean favorito=esfavorito.isSelected();
+				 String genero=GrupoGenero();
+				 String grupo=grupocombo.getValue();
+				 String notasstring=notas_ad.getText();
+				 LocalDate fecha = fechanac.getValue();
+				 modificado.setNombre(nombre);
+				 modificado.setTelefono(telefono);
+				 modificado.setEmail(email);
+				 modificado.setFavorito(favorito);
+				 modificado.setGenero(genero);
+				 modificado.setGrupo(grupo);
+				 modificado.setNotas(notasstring);
+				 modificado.setFechanac(fecha);
+				 System.out.println("llego al final");
+				 RemplazarDeLaLista(con,modificado);
+				 }
+			 
+			 }		 
+		 //comparar con la lista de objetos en observable list	 
+		 //remplazar esa posicion con el nuevo contacto modificado
+	 }
+
+	 public boolean RemplazarDeLaLista(Contacto antiguo,Contacto modificado) {
+		 boolean val=true;
+		 ObservableList<Contacto> listacontactos = FXCollections.observableArrayList();
+		 listacontactos=CargarDatos();
+		 for(Contacto c:listacontactos) {
+			 //el planteamiento parece estar bien  pero tengo que hacer la condicional diferente , los objetos llegan de forma correcta
+			 if(c.equals(antiguo)) {
+				 listacontactos.remove(c);				 
+			 }			 
+		 }
+		 listacontactos.add(modificado);
+		 if(listacontactos.contains(modificado)) {
+				 val=true;
+				 	System.out.println("la lista nueve el nuevo contacto");
+			 }
+			 else {
+				 System.out.println("no se ha modificado el contacto");
+				 val=false;
+			 }
+		 return val;
+	 }
+	 
 }
